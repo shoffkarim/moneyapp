c9f31da8b8bf2a2718cc5c1174f0340b4e24fe05
shoffkarim <k4r1mch1k@gmail.com>
2022-03-10 10:58:38 +0500

    setup project and add main page

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './main.sass'
+
+export const Main: React.FC = () => {
+  return (
+    <main className='main'>
+      <div className='wrapper'>
+        </div>
+    </main>
+  )
+}
