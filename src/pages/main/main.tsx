6e59b5bd762bcc36e0f4f743490210a83d97ed61
shoffkarim <k4r1mch1k@gmail.com>
2023-02-18 16:38:11 +0500

    simple server and connection to db

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -5,20 +5,40 @@ import "./main.sass";
 import { CardsContainer } from "components/cardsContainer";
 import { TransactionContainer } from "components/transactionContainer";
 import { History } from "components/history/historyContainer";
+import axios from "axios";
 
 
 export const Main: React.FC = () => {
+  const handleRequest = async () => {
+    try {
 
+      const response = await fetch('/api/auth/register', {
+        method: 'POST',
+        body: JSON.stringify({email: "karimshoff@gmail.com", password: "1234"}),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
+          'Access-Control-Allow-Credentials': 'true'
+        }
+      })
+      const data = await response
+      return data
+    } catch (error) {
+      console.log("Request Error", error)
+    }
+  }
   return (
     <div className="container">
-      <main className="main">
+      <button onClick={() => handleRequest()}>{"Create new user"}</button>
+      {/* <main className="main">
         <div className="wrapper">
           <CardsContainer/>
         </div>
         <TransactionContainer/>
-        <History/>
-      </main>
-      <Menu />
+        {/* <History/> */}
+      {/* </main> */}
+      {/* <Menu /> */}
     </div>
   );
 };
---
+++ src/server/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = config.get('port') || 5000
+
+app.use(express.json({extended: true}))
+app.use('/api/auth', require('./routes/auth.router'))
+
+async function start() {
+
+  try {
+    mongoose.set('strictQuery', false)
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    app.listen(PORT, () => console.log(`App started on ${PORT}`))
+
+  } catch (error) {
+    console.log('Server error', error)
+    process.exit(1)
+  }
+}
+
+start()
