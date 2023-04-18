967ed0eac8e69215d867584a3667edfa698f9b35
Карим Шарафутдинов <KRSharafutdinov@sberbank.ru>
2023-04-18 17:03:06 +0500

    fix calendar and add deps

--- src/pages/calendar/calendar.tsx
+++ src/pages/calendar/calendar.tsx
@@ -2,5 +2,5 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import React from 'react'
 
-export const Calendar = () =>
+export const Calendar: React.FC = () =>
   <FullCalendar plugins={[dayGridPlugin]} initialView='dayGridMonth'/>
